e56358ccf70d2b3db5d3dc4dc0612859289bb47c
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-08-20 01:34:30 -0300

    FP02

--- java-functional/src/programming/FP02Structured.java
+++ java-functional/src/programming/FP02Structured.java
@@ -1,16 +1,24 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class FP01Structured {
+public class FP02Structured {
     public static void main(String[] args) throws Exception {
         printAllNumbersInListStructured(Arrays.asList(12, 13, 14, 15, 16, 17, 18));
     }
 
+    private static void print(int number){
+        System.out.println(number);
+    }
+
     private static void printAllNumbersInListStructured(List<Integer> numbers){
+        //functional prog
+        numbers.stream()
+            .forEach(FP02Structured::print);
+
         //loop the numbers
-        for(int number: numbers){
-            System.out.println(number);
-        }
+        // for(int number: numbers){
+        //     System.out.println(number);
+        // }
     }
 
 }
